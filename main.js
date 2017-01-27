9ee6cde1e10ea12a99bb8fd084c17642b949ec2a
Ryo Oshima <oshimaryo@gmail.com>
2017-01-28 00:27:00 +0900

    split js

---
+++ main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+  const DAYS = ['日', '月', '火', '水', '木', '金', '土'];
+  const clock = () => {
+    const now = new Date();
+    const current = {
+      'year': now.getFullYear(),
+      'month': (now.getMonth() + 1 < 10) ? '0' + (now.getMonth() + 1) : now.getMonth() + 1,
+      'date': now.getDate(),
+      'day': DAYS[now.getDay()],
+      'hour': (now.getHours() < 10) ? '0' + now.getHours() : now.getHours(),
+      'minute': (now.getMinutes() < 10) ? '0' + now.getMinutes() : now.getMinutes()
+    };
+    for(key in current){ document.getElementById(key).innerHTML = current[key]; }
+  };
+
+  setInterval(clock, 100);
+});
