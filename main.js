9b9f6f41b1cdced512806e6d3a3ce8c6efe49e81
Ryo Oshima <oshimaryo@gmail.com>
2017-06-08 15:42:04 +0900

    add zeroPadding

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ document.addEventListener('DOMContentLoaded', function(e){
   var DAYS = ['日', '月', '火', '水', '木', '金', '土'];
   var clock = function(){
     var now = new Date();
+    var zeroPadding = function(val) {return (val < 10) ? '0' + val : val;};
     var current = {
       'year': now.getFullYear(),
-      'month': (now.getMonth() + 1 < 10) ? '0' + (now.getMonth() + 1) : now.getMonth() + 1,
-      'date': now.getDate(),
+      'month': zeroPadding(now.getMonth() + 1),
+      'date': zeroPadding(now.getDate()),
       'day': DAYS[now.getDay()],
-      'hour': (now.getHours() < 10) ? '0' + now.getHours() : now.getHours(),
-      'minute': (now.getMinutes() < 10) ? '0' + now.getMinutes() : now.getMinutes()
+      'hour': zeroPadding(now.getHours()),
+      'minute': zeroPadding(now.getMinutes())
     };
     for(key in current){ document.getElementById(key).innerHTML = current[key]; }
   };
